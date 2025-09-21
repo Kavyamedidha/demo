6d62063fec4a2a118f3b21a6aa572e0fd4d036b2
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-09-21 11:04:35 +0530

    DataTypes

---
+++ teegala/src/com/tnsif/first/DataTypesDemo.java
@@ -0,0 +1,27 @@
+package com.tnsif.first;
+class DataTypeDemo {
+    public static void main(String[] args) {
+        byte bytevar = 100;
+        int myvar = 6;
+        short shortvar = 10000;
+        long longvar = 10000000L;
+        float floatvar = 3.22f;
+        double doublevar = 2.4454676;
+        char charvar = 'R';
+        boolean booleanvar = true;
+        String Stringvar = "Rohika";
+
+        System.out.println("=== Primitive Datatypes ===");
+        System.out.println("byte value: " + bytevar);
+        System.out.println("int value: " + myvar);
+        System.out.println("short value: " + shortvar);
+        System.out.println("long value: " + longvar);
+        System.out.println("float value: " + floatvar);
+        System.out.println("double value: " + doublevar);
+        System.out.println("char value: " + charvar);
+        System.out.println("boolean value: " + booleanvar);
+
+        System.out.println("=== Non-Primitive Datatypes ===");
+        System.out.println("String value: " + Stringvar);
+    }
+}

430e400f50515b4d1752e9477793bd1a4b889229
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-09-21 12:15:42 +0530

    EvenOrOdd

---
+++ teegala/src/com/tnsif/second/EvenOrOdd.java
@@ -0,0 +1,11 @@
+package com.tnsif.second;
+
+class EvenOrOdd{
+    public static void main(String[]args)
+    {
+        int number = 20;
+
+        if(number % 2 == 0)
+        System.out.println(  20+ " :is an even number" );
+}
+}

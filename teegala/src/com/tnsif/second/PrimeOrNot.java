e3c0fc0ab7a5f03d5f141f57b17ef80952c103db
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-09-21 12:21:57 +0530

    PrimeOrNot

---
+++ teegala/src/com/tnsif/second/PrimeOrNot.java
@@ -0,0 +1,25 @@
+package com.tnsif.second;
+
+class PrimeOrNot {
+    public static void main(String[] args) {
+        int n = 27;
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(n + " is a prime number");
+        } else {
+            System.out.println(n + " is not a prime number");
+ }
+}
+}

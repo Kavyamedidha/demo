d09a59105cb22fdea4f643be98b8e2e597988de8
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-07 18:39:34 +0530

    Implicity

---
+++ teegala/src/com/tnsif/Inclass/Implicity.java
@@ -0,0 +1,14 @@
+package com.tnsif.Inclass;
+
+public class Implicity {
+
+	    public static void main(String[]args)
+	    {
+	        int num = 100;
+	        double d=num;
+	        System.out.println(d);
+	    }
+	    
+	}
+
+

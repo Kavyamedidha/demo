9bc46cf6cc2eaf70341d7b4798ffc718ad1ca5da
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-08 17:43:26 +0530

    shape

---
+++ teegala/src/com/tnsif/abstration/Shape.java
@@ -0,0 +1,11 @@
+package com.tnsif.abstration;
+
+public abstract class Shape {
+	protected float area;
+	abstract void calArea();
+	public void show() {
+		System.out.println("The area of shape is:"+area);
+	}
+	
+
+}

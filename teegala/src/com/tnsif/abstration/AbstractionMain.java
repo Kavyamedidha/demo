17559096596a0ab52a6efac83477e90dea8b17e0
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-08 17:40:43 +0530

    AbstractionMain

---
+++ teegala/src/com/tnsif/abstration/AbstractionMain.java
@@ -0,0 +1,18 @@
+package com.tnsif.abstration;
+
+public class AbstractionMain {
+
+	public static void main(String[] args) {
+		
+			Square s=new Square(4);
+			s.calArea();
+			s.show();
+			Rectangle r=new Rectangle(4,5);
+			r.calArea();
+			r.show();	
+		}
+	}
+
+	
+
+

bf7e8909f44aa3cfd1d5686a1dfe25dd169bf8d9
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-08 22:16:26 +0530

    Singleinheritancemain

---
+++ teegala/src/com/tnsif/inheritance/SingleInheritenceMain.java
@@ -0,0 +1,14 @@
+package com.tnsif.inheritance;
+
+
+ class SingleInheritanceMain {
+	public static void main(String[] args) {
+		B obj = new B();
+		obj.display();
+		System.out.println(obj.a);
+		obj.show();
+	}
+
+
+
+}

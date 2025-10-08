f475842ab3e46e7d2681eb4f0aeec5d9ef3a7e93
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-08 17:42:39 +0530

    Rectangle

---
+++ teegala/src/com/tnsif/abstration/Rectangle.java
@@ -0,0 +1,13 @@
+package com.tnsif.abstration;
+
+public class Rectangle extends Shape {
+	private int height=2,width=10;
+	public Rectangle(int height,int width) {
+		this.height=height;
+		this.width=width;
+	}
+        void calArea() {
+        	super.area=height*width;
+         
+
+}}

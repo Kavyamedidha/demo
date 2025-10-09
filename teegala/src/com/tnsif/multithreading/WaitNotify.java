125eb1620c2c156bc5ab1c54b94f5f5c50a8008f
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-09 19:18:23 +0530

    waitnotify

---
+++ teegala/src/com/tnsif/multithreading/WaitNotify.java
@@ -0,0 +1,21 @@
+package com.tnsif.multithreading;
+
+public class WaitNotify {
+	
+	    public static void main(String[] args) {
+	        SharedResource resource = new SharedResource();
+
+	        Thread thread = new Thread(() -> resource.waitForNotify());
+	        thread.start();
+
+	        try {
+	            Thread.sleep(2000); // Let the thread wait
+	        } catch (InterruptedException e) {
+	            Thread.currentThread().interrupt();
+	        }
+
+	        resource.doNotify();
+	    }
+	}
+
+

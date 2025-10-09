0ec23a2d92d3ab48c3924d002995281f5664edba
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-09 19:15:58 +0530

    runnableinf

---
+++ teegala/src/com/tnsif/multithreading/runnableInf.java
@@ -0,0 +1,21 @@
+package com.tnsif.multithreading;
+
+
+
+ class RunnableInf implements Runnable {
+		public void run() {
+			
+			for(int i=1;i<=5;i++) {
+			
+				try {
+					System.out.println("Hello");
+					Thread.sleep(2000);
+				}catch(Exception e){
+					System.out.println(e);
+					
+				}
+			}
+		}
+	}
+
+

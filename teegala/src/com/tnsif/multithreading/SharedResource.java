f45ec1e820526408e25d205c4f90c3c149212f61
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-09 19:16:58 +0530

    sharedresource

---
+++ teegala/src/com/tnsif/multithreading/SharedResource.java
@@ -0,0 +1,22 @@
+package com.tnsif.multithreading;
+
+
+	class SharedResource {
+	    public synchronized void waitForNotify() {
+	        try {
+	            System.out.println("Thread is waiting...");
+	            wait();
+	            System.out.println("Thread is notified.");
+	        } catch (InterruptedException e) {
+	            Thread.currentThread().interrupt();
+	        }
+	    }
+
+	    public synchronized void doNotify() {
+	        System.out.println("Notifying thread...");
+	        notify();
+	    }
+	}
+
+
+

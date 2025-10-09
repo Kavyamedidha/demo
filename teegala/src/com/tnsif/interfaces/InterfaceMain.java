dc6f3fe59baaa2211220bbc99ed61eac93cd927c
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-09 17:14:26 +0530

    interfacemain

---
+++ teegala/src/com/tnsif/interfaces/InterfaceMain.java
@@ -0,0 +1,16 @@
+package com.tnsif.interfaces;
+
+public class InterfaceMain  {
+	public static void main(String[] args) {
+		Jio j=new Jio();
+		j.call();
+		j.sms();
+		Samsang s=new Samsang();
+		s.call();
+		s.sms();
+		
+	}
+
+}
+
+

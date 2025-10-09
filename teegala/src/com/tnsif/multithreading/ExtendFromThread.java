fbfc6c230baec5d791e1631bb30e3299ef5d7f8f
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-09 19:11:24 +0530

    extendfromthread

---
+++ teegala/src/com/tnsif/multithreading/ExtendFromThread.java
@@ -0,0 +1,17 @@
+
+	package com.tnsif.multithreading;
+
+	public class ExtendFromThread {
+		public static void main(String args[]) {
+			ThreadDemo d=new ThreadDemo();
+			d.setPriority(1);
+			d.start();
+			ThreadDemo d1=new ThreadDemo();
+			d1.setPriority(10);
+			
+		}
+		
+
+	}
+
+

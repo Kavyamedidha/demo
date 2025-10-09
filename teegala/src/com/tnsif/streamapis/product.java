fb9fc30950ddf4c1942ac08405eab17ed3088bf5
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-09 19:25:48 +0530

    product

---
+++ teegala/src/com/tnsif/streamapis/product.java
@@ -0,0 +1,19 @@
+package com.tnsif.streamapis;
+
+	//demo for stream api 
+
+	 class Product {
+		int id;
+		String name;
+		float price;
+		
+		// parameter constructor 
+		
+		public Product(int id, String name, float price) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.price = price;
+		}
+	
+}

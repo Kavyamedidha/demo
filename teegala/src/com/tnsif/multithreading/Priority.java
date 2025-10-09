7b594206f74ea2c9f71565c436e7f64a11d7be7e
AutoRABIT <AutoRABIT@DESKTOP-RK2BAHB>
2025-10-09 19:14:24 +0530

    priority

---
+++ teegala/src/com/tnsif/multithreading/Priority.java
@@ -0,0 +1,17 @@
+package com.tnsif.multithreading;
+
+
+
+	public class Priority {
+		public static void main(String args[]) {
+			MultiPriority m1=new MultiPriority();
+			MultiPriority m2=new MultiPriority();
+			m1.setPriority(1);
+			m2.setPriority(10);
+			m1.start();
+			m2.start();
+			
+		}
+
+	}
+
